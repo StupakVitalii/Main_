b3135f99f6735dce8594fee7e9805a33913d11e0
Vital <iamvitalos@gmail.com>
2023-10-16 12:44:19 +0300

    Home work. Lesson7. Ex1-4

--- Lesson_7/Ex7_2.py
+++ Lesson_7/Ex7_2.py
@@ -1,34 +1,16 @@
 def season(number):
-    month = ['Jenuary', 'February', 'March', 'April', 'May', 'June',
-               'July', 'August', 'September', 'October',
-               'November', 'December']
-    if number == 1:
+    month = ['Winter', 'Spring', 'Summer', 'Autumn']
+    if number in range(1, 3) or number == 12:
         print(month[0])
-    elif number == 2:
+    elif number in range(3, 6):
         print(month[1])
-    elif number == 3:
+    elif number in range(6, 9):
         print(month[2])
-    elif number == 4:
+    elif number in range(9, 12):
         print(month[3])
-    elif number == 5:
-        print(month[4])
-    elif number == 6:
-        print(month[5])
-    elif number == 7:
-        print(month[6])
-    elif number == 8:
-        print(month[7])
-    elif number == 9:
-        print(month[8])
-    elif number == 10:
-        print(month[9])
-    elif number == 11:
-        print(month[10])
-    elif number == 12:
-        print(month[11])
     else:
         print('Місяця з таким номером не існує !')
     return ('')
 
-func_2 = season(8)
+func_2 = season(12)
 print(func_2)
